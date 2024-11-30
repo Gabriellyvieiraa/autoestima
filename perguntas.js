637dc1979d445a376ab5d26f81b492dd0fa75bb3
GabriellyVieira <00001115746601sp@al.educacao.sp.gov.br>
2024-11-30 18:36:15 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Influências',
+    'Quais mulheres influentes são ícones de autoestima e empoderamento?',
+    'Rihanna: Defende a aceitação corporal e inspira mulheres com sua autoconfiança. <br><br>Malala Yousafzai: Lutou pelo direito à educação e inspira mulheres a acreditar em sua capacidade de mudar o mundo.'
+)
+
+criaCartao(
+    'Autoestima',
+    'O que é autoestima e por que é importante para as mulheres?',
+    'A autoestima é a percepção que uma pessoa tem de si mesma, incluindo sua aparência, capacidades e valor. Para as mulheres, é crucial, pois influencia como elas se sentem, tomam decisões e enfrentam desafios em um mundo que frequentemente impõe padrões irreais de beleza e comportamento.'
+)
+
+criaCartao(
+    'Sociedade',
+    'Como a sociedade impacta a autoestima feminina?',
+    'A sociedade pode impactar negativamente a autoestima das mulheres ao promover padrões de beleza inatingíveis, comparar mulheres constantemente e desvalorizar conquistas femininas em diversas áreas. No entanto, movimentos de aceitação corporal e empoderamento têm ajudado a mudar essa realidade.'
+)
+
+criaCartao(
+    'Práticas',
+    'Quais práticas ajudam a melhorar a autoestima?',
+    'Cuidar da saúde física e mental: Alimentação, exercício e descanso adequados.<br><br>Reconhecer suas conquistas: Pequenas ou grandes vitórias.<br><br>Estabelecer limites: Proteger-se de críticas destrutivas e pessoas tóxicas.<br><br>Valorizar o autoconhecimento: Aceitar e celebrar suas particularidades.'
+)
